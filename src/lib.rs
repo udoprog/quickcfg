2c5ae900a9b083129189536e6c3cdcb9d2d5e63e
John-John Tedro <udoprog@tedro.se>
2018-11-22 10:10:11 +0100

    Re-organized modules

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ mod command;
 mod config;
 pub mod environment;
 pub mod facts;
-mod file;
+mod file_operations;
 pub mod git;
 pub mod hierarchy;
 pub mod opts;
@@ -16,7 +16,7 @@ mod template;
 pub mod unit;
 
 pub use crate::config::Config;
-pub use crate::file::{Load, Save};
+pub use crate::file_operations::{Load, Save};
 pub use crate::state::{DiskState, State};
 pub use crate::system::SystemInput;
 pub use crate::template::Template;
